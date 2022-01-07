729771a1ab5ea7f8dcf693d7435f3bb751fb12d5
elbarqy <elbarky.dev@gmail.com>
2022-01-07 19:22:08 +0200

    database singleton implementation

--- app/src/main/java/com/barqy/mvvm/data/dao/UserDao.kt
+++ app/src/main/java/com/barqy/mvvm/data/dao/UserDao.kt
@@ -1,14 +1,17 @@
 package com.barqy.mvvm.data.dao
 
 import androidx.room.*
+import com.barqy.mvvm.data.models.Task
 import com.barqy.mvvm.data.models.User
+import com.barqy.mvvm.data.models.crossRef.UserTaskCrossRef
+import com.barqy.mvvm.data.models.relations.UserWithTasks
 
 @Dao
 interface UserDao {
     @Query("SELECT * FROM user")
     fun getAll(): List<User>
 
-    @Query("Select * FROM user WHERE uid IN (:userIds)")
+    @Query("Select * FROM user WHERE userID IN (:userIds)")
     fun loadAllById(userIds: IntArray): List<User>
 
     @Query(
@@ -16,8 +19,19 @@ interface UserDao {
                 "last_name LIKE :last LIMIT 1"
     )
     fun findByName(first: String, second: String): User
+
+    @Transaction
+    @Query("SELECT * FROM user")
+    fun getUsersWithTasks(): List<UserWithTasks>
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertAll(vararg users: User)
+    fun insertTask(task: Task)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertUserTaskCrossRef(userTaskCrossRef: UserTaskCrossRef)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertUser(user: User)
 
     @Delete
     fun delete(user: User)
--- app/src/main/java/com/barqy/mvvm/data/database.kt
+++ app/src/main/java/com/barqy/mvvm/data/database.kt
@@ -1,11 +1,31 @@
 package com.barqy.mvvm.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.barqy.mvvm.data.dao.UserDao
+import com.barqy.mvvm.data.models.Task
 import com.barqy.mvvm.data.models.User
+import com.barqy.mvvm.data.models.crossRef.UserTaskCrossRef
 
-@Database(entities = [User::class], version = 1)
-abstract class AppDatabase : RoomDatabase() {
+@Database(entities = [User::class, Task::class, UserTaskCrossRef::class], version = 1)
+abstract class UserDataBase : RoomDatabase() {
     abstract fun userDao(): UserDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: UserDataBase? = null
+        fun getInstance(context: Context): UserDataBase {
+            synchronized(this) {
+                return INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    UserDataBase::class.java,
+                    "user_db",
+                ).build().also {
+                    INSTANCE = it
+                }
+            }
+        }
+    }
 }
